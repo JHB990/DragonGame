c4691ca15267da58d50c21b239e5ebad6a448ca5
Jose Hans Bautista <Jazz.j.h.r@gmail.com>
2022-01-03 22:44:28 -0600

    Update DragonGame.py
    Add the battle function, create  arandom limit of power of the enemy. change game() to game.gameStart() to call the events

--- DragonGame.py
+++ DragonGame.py
@@ -1,31 +1,47 @@
 import random
 
 class Dragon:
-    def __init__(self, name, kind, level, stats):
+    def __init__(self, name, kind, level, live):
         self.name = name
         self.kind = kind
         self.level = level
-        self.stats = stats
+        self.live = live
     def player(self):
         print('\tHello ', self.name, '\n\t\tYou are a : ', self.kind ,' Dragon\n\t\tYour level is ', self.level, '\n\t\tYour stats are: ', self.stats)
 
 
 class Troll:
-    def __init__(self):
-        stats = {
-            "level" : 10,
-            "punch" : 5,
-            "live" : 4,
-            "back" : 2
-        }
+    def __init__(self, level, punch, live, back):
+        self.level = level
+        self.punch = punch
+        self.live = live
+        self.back = back
+    def enemy(self):
+        print('\n\tTroll: \n\t', self.level, '\n\t\t', self.punch, '\n\t\t', self.live)
 
 
-def game():
-    print('Dragon')
-    print('please enter the information of your dragon: ')
-    dragon = Dragon(input('Name: '), input('kind: '), 1, 1)
-    dragon.player()
+class game(Dragon, Troll):
+    def gameStart():
+        print('Dragon')
+        print('please enter the information of your dragon: ')
+        dragon = Dragon(input('Name: '), input('kind: '), 1, 1)
+        dragon.player()
+    def battle():
+        a = 0
+        b = 0
+        c = 0
+        d = 0
+        a = random.randint(1, 4)
+        b = random.randint(1, 4)
+        c = random.randint(1, 4)
+        d = random.randint(1, 4)
+        troll = Troll(a,b,c,d)
+        troll.enemy()
+
+        
 
 
-game()
+game.gameStart()
 print('Wlecome to Ralthed the kingdom of the Imperial Dragons of the North, \n You live on Ascencia the capital of the Kingdom')
+print('just for try you see a Troll on the middle of the city just in fornt of the castle, you decide to defend the kingdom, the troll wants to destroy everything')
+game.battle()
